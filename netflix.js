c08a8597586006128af198c9323f7598dd036d19
prince bhadana <princebhadana0575@gmail.com>
2026-05-15 09:08:01 +0530

    Fix attachment file paths for GitHub Pages

--- netflix.js
+++ netflix.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let signinBtn = document.querySelector("#sign");
 let login = document.querySelector(".signin-modal");
 let crossBtn = document.querySelector("#cross");
@@ -17,3 +18,24 @@ allQuestion.forEach((item, index) => {
     item.lastElementChild.classList.toggle("rotate");
   });
 });
+=======
+let signinBtn = document.querySelector("#sign");
+let login = document.querySelector(".signin-modal");
+let crossBtn = document.querySelector("#cross");
+
+signinBtn.addEventListener("click", () => {
+  login.classList.add("showmodal");
+});
+crossBtn.addEventListener("click", () => {
+  login.classList.remove("showmodal");
+});
+
+let allQuestion = document.querySelectorAll(".faqitems h3");
+
+allQuestion.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    item.nextElementSibling.classList.toggle("hide");
+    item.lastElementChild.classList.toggle("rotate");
+  });
+});
+>>>>>>> 4d144a6 (Fix attachment file paths for GitHub Pages)
