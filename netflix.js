c1b8b7955a649d4f93199f592959b61dd4370fe3
prince bhadana <princebhadana0575@gmail.com>
2026-05-15 09:31:51 +0530

    sab kukh ache se mange kiya h

--- netflix.js
+++ netflix.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let signinBtn = document.querySelector("#sign");
 let login = document.querySelector(".signin-modal");
 let crossBtn = document.querySelector("#cross");
@@ -18,24 +17,3 @@ allQuestion.forEach((item, index) => {
     item.lastElementChild.classList.toggle("rotate");
   });
 });
-=======
-let signinBtn = document.querySelector("#sign");
-let login = document.querySelector(".signin-modal");
-let crossBtn = document.querySelector("#cross");
-
-signinBtn.addEventListener("click", () => {
-  login.classList.add("showmodal");
-});
-crossBtn.addEventListener("click", () => {
-  login.classList.remove("showmodal");
-});
-
-let allQuestion = document.querySelectorAll(".faqitems h3");
-
-allQuestion.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    item.nextElementSibling.classList.toggle("hide");
-    item.lastElementChild.classList.toggle("rotate");
-  });
-});
->>>>>>> 4d144a6 (Fix attachment file paths for GitHub Pages)
